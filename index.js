f33bcf94a4552e7262b8859f292cc073d27a4d3b
Gana <127880189+Gagana30@users.noreply.github.com>
2023-07-19 20:05:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const db = require('./db');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
